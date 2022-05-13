47a297713d1a3d8d1776a76267b2d4fdf3b74d62
jang jae min <wkwovlf12@naver.com>
2022-05-13 16:05:54 +0900

    22.5.13

---
+++ src/Test5.java
@@ -0,0 +1,13 @@
+//백준 코테 1008번 + 메인 클래스 네임은 Main으로 설정 !
+
+import java.util.Scanner;
+
+public class Test5 {
+    public static void main(String[] args) {
+        Scanner s1 = new Scanner(System.in);
+        double a = s1.nextInt();
+        double b = s1.nextInt();
+
+        System.out.println(a / b); // 1 / 3 = 0.3333333333333333
+    }
+}
--- src/Test6.java
+++ src/Test6.java
@@ -1,12 +1,13 @@
-//백준 코테 1001번 + 메인 클래스 네임은 Main으로 설정 !
+// 백준 10869번 문제 / Main !
+
 import java.util.Scanner;
 
-public class Test3 {
+public class Test6 {
     public static void main(String[] args) {
         Scanner s1 = new Scanner(System.in);
         int a = s1.nextInt();
         int b = s1.nextInt();
 
-        System.out.println(a-b);
+
     }
 }
