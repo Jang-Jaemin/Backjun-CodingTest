75865bc83df095142d495c940feebbf92d69de66
jang jae min <wkwovlf12@naver.com>
2022-05-13 15:27:21 +0900

    Test3

---
+++ src/Test3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Test3 {
+    public static void main(String[] args) {
+        Scanner s1 = new Scanner(System.in);
+        int a = s1.nextInt();
+        int b = s1.nextInt();
+
+        System.out.println(a-b);
+    }
+}
