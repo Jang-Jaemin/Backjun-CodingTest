222d96d0f7802e4b56aa38f59ff1b39c809da273
jang jae min <wkwovlf12@naver.com>
2022-05-13 11:50:32 +0900

    first

---
+++ src/Test1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Test1 {
+    public static void main(String[] args) {
+        Scanner s1 = new Scanner(System.in);
+        int a = s1.nextInt();
+        int b = s1.nextInt();
+
+        System.out.println(a+b);
+
+    }
+}
