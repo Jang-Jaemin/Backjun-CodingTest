24dc151195de3762e1f515d3e086c641c2e14414
jang jae min <wkwovlf12@naver.com>
2022-05-14 18:47:08 +0900

    22.5.14

---
+++ src/Test8.java
@@ -0,0 +1,10 @@
+// 백준 18108번 문제.
+import java.util.Scanner;
+
+public class Test8 {
+    public static void main(String[] args) {
+        Scanner s1 = new Scanner(System.in);
+        int a = s1.nextInt();
+        System.out.println(a - 543); // ex) 불기 연도 - 서기 연도 = 2541 - 1998 = 543
+    }
+}
