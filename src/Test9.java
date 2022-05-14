4cdccb67863e635cbe0d5203f61b007f3e5d0c0a
jang jae min <wkwovlf12@naver.com>
2022-05-14 18:58:17 +0900

    22.5.14

---
+++ src/Test9.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Test9 {
+    public static void main(String[] args) {
+        Scanner s1 = new Scanner(System.in);
+        // 프로그래밍에서 나머지는 % 로 나타낸다.
+        // ex) 17 % 3 = 2
+        int A = s1.nextInt();
+        int B = s1.nextInt();
+        int C = s1.nextInt();
+
+        System.out.println((A+B)%C);
+        System.out.println((A % C + B % C) % C);
+        System.out.println((A * B) % C);
+        System.out.println((A % C * B % C) % C);
+
+
+    }
+}
