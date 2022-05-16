1df7e17292122a3a22aeeb7fcdc8606c8548fa6c
jang jae min <wkwovlf12@naver.com>
2022-05-16 17:57:23 +0900

    22.5.16

---
+++ src/Test18.java
@@ -0,0 +1,14 @@
+// 백준 코테 : 15552 번
+// BufferedReader와 BufferedWriter
+// 스캐너 대신 BufferedReader와 BufferedWriter를 사용하여 풀어보는 문제.
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+
+public class Test18 {
+    public static void main(String[] args) {
+        BufferedReader s1 = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
