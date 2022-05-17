f0876afa86cd37942430b4f65671d370ccbfa50f
jang jae min <wkwovlf12@naver.com>
2022-05-17 16:30:54 +0900

    22.5.17

---
+++ src/Test19.java
@@ -0,0 +1,16 @@
+// 백준 알고리즘 : 2741 번.
+
+
+import java.util.Scanner;
+
+public class Test19 {
+    public static void main(String [] args){
+        Scanner s1 = new Scanner(System.in);
+
+        int a = s1.nextInt();
+
+        for(int i=1; i <= a; i++){
+            System.out.println(i);
+        }
+    }
+}
