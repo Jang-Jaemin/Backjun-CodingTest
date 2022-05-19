aa7d74149df1ca66e82c576cc6f9721da9d29dc7
jang jae min <wkwovlf12@naver.com>
2022-05-19 16:32:00 +0900

    22.5.19

--- src/Test23.java
+++ src/Test23.java
@@ -6,5 +6,11 @@ import java.util.Scanner;
 public class Test23 {
     public static void main(String[] args) {
         Scanner s1 = new Scanner(System.in);
+            int lines = 10;
+                for(int i = 1; i <= lines; i++){
+                    for(int j = 1; j <= i; j++){
+                        System.out.println("*");
+                    }
+                } System.out.println();
     }
 }
