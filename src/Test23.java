342a3912e02a3a4dac8724d32c9bbcc86b1054f5
jang jae min <wkwovlf12@naver.com>
2022-05-19 16:54:27 +0900

    22.5.19

--- src/Test23.java
+++ src/Test23.java
@@ -1,16 +1,15 @@
-// 백준 알고리즘 :
-
-
 import java.util.Scanner;
 
 public class Test23 {
     public static void main(String[] args) {
         Scanner s1 = new Scanner(System.in);
-            int lines = 10;
-                for(int i = 1; i <= lines; i++){
-                    for(int j = 1; j <= i; j++){
-                        System.out.println("*");
-                    }
-                } System.out.println();
+        int a = s1.nextInt();
+
+        for(int i = 1; i <= a; i++) { // a까지
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*"); // println을 없애야 트리 모양 나온다 주의 !!
+            }
+            System.out.println();
+        }
     }
 }
