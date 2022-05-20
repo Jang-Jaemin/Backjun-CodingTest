aba6f54a02dcdcf6480bb3cf8d99d0e7fe478a48
jang jae min <wkwovlf12@naver.com>
2022-05-20 23:35:41 +0900

    22.5.20

--- src/Test25.java
+++ src/Test25.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Test25 {
     public static void main(String[] args) {
+        Scanner s1 = new Scanner(System.in);
+
+        int a = s1.nextInt();
+
 
     }
 }
