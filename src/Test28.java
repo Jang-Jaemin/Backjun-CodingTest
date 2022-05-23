a49fc5a24a14f354668b8c23c89a4f1f883d350d
jang jae min <wkwovlf12@naver.com>
2022-05-23 22:35:17 +0900

    22.5.23

---
+++ src/Test28.java
@@ -0,0 +1,11 @@
+// 백준 알고리즘 :
+
+import java.util.Scanner;
+
+public class Test28 {
+    public static void main(String[] args) {
+        Scanner s1 = new Scanner(System.in);
+
+        int a = s1.nextInt();
+    }
+}
