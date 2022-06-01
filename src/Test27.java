bd173041a1480793a3b7c04e375d0997a120d774
jang jae min <wkwovlf12@naver.com>
2022-06-01 23:22:18 +0900

    22.6.1

--- src/Test27.java
+++ src/Test27.java
@@ -6,6 +6,15 @@ public class Test27 {
     public static void main(String[] args) {
         Scanner s1 = new Scanner(System.in);
 
-        int a = s1.nextInt();
+        //hasNexint는 입력값이 정수이다면 true 반환, 정수가 아닐 경우에는 예외를 발생시키고 false
+        //를 반환하며 반복문이 종료된다.
+        while (s1.hasNextInt()){
+            int a,b;
+            a = s1.nextInt();
+            b = s1.nextInt();
+
+            System.out.println(a+b);
+        }
+        s1.close();
     }
 }
