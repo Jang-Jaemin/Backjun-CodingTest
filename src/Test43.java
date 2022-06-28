8a050d80c397173392cbf3300d3eda956ce62ba3
jang jae min <wkwovlf12@naver.com>
2022-06-28 22:52:25 +0900

    22.6.28

---
+++ src/Test43.java
@@ -0,0 +1,14 @@
+//백준 문자열 :
+
+
+import java.util.Scanner;
+
+public class Test43 {
+    public static void main(String[] args) {
+        Scanner s1 = new Scanner(System.in);
+
+        int a = s1.nextInt();
+
+
+    }
+}
