4ed6177803428e2bccd7fec28dad469397bb2601
jang jae min <wkwovlf12@naver.com>
2022-07-07 18:24:39 +0900

    22.7.7

---
+++ src/No_2292.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class No_2292 {
+    public static void main(String[] args) {
+        Scanner s1 = new Scanner(System.in);
+
+        int a = s1.nextInt();
+
+
+    }
+}
